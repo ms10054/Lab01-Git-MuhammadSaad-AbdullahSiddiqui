a13b1d9602c5d7f12fb1e56c908735092d84281c
ms10054 <ms10054@st.habib.edu.pk>
2026-02-12 12:20:26 +0500

    commit

--- Lab4/task1.s
+++ Lab4/task1.s
@@ -1,12 +1,12 @@
 .text
 .globl main
 main:
-    addi x5, x0, 8
-    addi x6, x0, 1
+    addi x5, x0, 8 # n = 8
+    addi x6, x0, 1  
 
     loop:
-        mul x6, x6, x5
-        addi x5, x5, -1
-        bne x5, x0, loop
+        mul x6, x6, x5  # x6 *= n
+        addi x5, x5, -1 # n--
+        bne x5, x0, loop # if n!=0, jump back to loop
 end:
     j end
--- Lab4/task3.s
+++ Lab4/task3.s
@@ -11,17 +11,18 @@ main:
     sw x5, 8(x10)
     li x5, 1
     sw x5, 12(x10)
+    #initial array = [8, 2, 10, 1]
 
     li x11, 4              # len = 4
 
-    li x5, 0               # i = 0 (outer loop index)
+    li x5, 0               # i = 0 
 
     outer_loop:
-        bge x5, x11, end       # if i >= len → end
+        bge x5, x11, end       # if i >= len - end
         add x6, x5, x0         # j = i
 
     inner_loop:
-        bge x6, x11, next_i    # if j >= len → next outer iteration
+        bge x6, x11, next_i    # if j >= len - next_i
 
         # Load a[i] into x7
         slli x12, x5, 2         # offset_i = i * 4
@@ -49,7 +50,8 @@ main:
         addi x5, x5, 1         # i++
         j outer_loop
 
+
 end:
-    j end                  
+    j end   #final array = [1, 2, 8, 10]               
               
 
